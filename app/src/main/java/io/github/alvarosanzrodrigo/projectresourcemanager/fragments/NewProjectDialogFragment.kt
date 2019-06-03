e7f6568885ab690c09878a6792475615d37eca6d
Alvaro <alvaro.sanz@live.u-tad.com>
2019-06-03 19:34:50 +0200

    wow, that was easy
    It creates a default folder on the storage for saving the data

--- app/src/main/java/io/github/alvarosanzrodrigo/projectresourcemanager/fragments/NewProjectDialogFragment.kt
+++ app/src/main/java/io/github/alvarosanzrodrigo/projectresourcemanager/fragments/NewProjectDialogFragment.kt
@@ -1,6 +1,7 @@
 package io.github.alvarosanzrodrigo.projectresourcemanager.fragments
 
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +11,9 @@ import android.widget.EditText
 import io.github.alvarosanzrodrigo.projectresourcemanager.R
 import io.github.alvarosanzrodrigo.projectresourcemanager.daoRepositories.ProjectDaoRepository
 import io.github.alvarosanzrodrigo.projectresourcemanager.models.Project
-import kotlinx.android.synthetic.main.fragment_dialog_new_project.view.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.util.*
 
 class NewProjectDialogFragment : DialogFragment(){
@@ -27,8 +30,23 @@ class NewProjectDialogFragment : DialogFragment(){
             newProjectName = findViewById(R.id.project_name_edit_text)
             saveButton = findViewById(R.id.save_project_name_button)
             saveButton.setOnClickListener {
-                if (newProjectName.text.toString() != ""){
-                    val projects = listOf(Project(newProjectName.text.toString(), date = Date()))
+                val projectName = newProjectName.text.toString()
+                if (projectName != ""){
+                    val projects = listOf(Project(projectName, date = Date()))
+                    // create a File object for the parent directory
+                    val projectDirectoryPath = Environment.getExternalStorageDirectory().path + "/ProjectResourceManager/"+projectName+"/"
+                    val projectDirectory = File(projectDirectoryPath)
+                    val picsDirectory = File(projectDirectoryPath+"pics")
+                    val videoDirectory = File(projectDirectoryPath+"video")
+                    val audioDirectory = File(projectDirectoryPath+"audio")
+                    val textDirectory = File(projectDirectoryPath+"text")
+                    // have the object build the directory structure, if needed.
+                    projectDirectory.mkdirs()
+                    picsDirectory.mkdirs()
+                    videoDirectory.mkdirs()
+                    audioDirectory.mkdirs()
+                    textDirectory.mkdirs()
+
                     activity?.application?.let { ProjectDaoRepository.getInstance(it).insertAll(projects) }
                 }
                 this@NewProjectDialogFragment.dismiss()
